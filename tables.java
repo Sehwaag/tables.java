dbb116549864e32c43565b3062b3dce851aa3041
Sehwaag <sehwaagdsehwaagd@gmail.com>
2024-10-17 20:03:17 +0530

    Create tables.java

---
+++ tables.java
@@ -0,0 +1,14 @@
+import java.util.*;https:
+class Main
+{
+	public static void main(String[] args) 
+	{
+		int n=1;
+	    for(int i=10;i>=n;i--)
+	    {
+	        int sum=i*5;
+	        System.out.println(sum );
+	        
+	    }
+	}
+}
